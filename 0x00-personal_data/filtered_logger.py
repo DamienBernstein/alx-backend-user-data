1ec4a66c6c9140e9030f48a22b2dc62e8fe36c1b
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2023-08-02 10:54:48 +0200

    Update filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -31,19 +31,19 @@ def obfuscate_message(fields: List[str], redaction: str,
 
 
 class RedactingFormatter(logging.Formatter):
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
-
-    def __init__(self, fields: List[str]):
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.fields = fields
-
-    def format(self, record: logging.LogRecord) -> str:
-        message = super(RedactingFormatter, self).format(record)
-        redacted = obfuscate_message(self.fields, self.REDACTION,
-                                     message, self.SEPARATOR)
-        return redacted
+      REDACTION = "***"
+      FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+      SEPARATOR = ";"
+  
+      def __init__(self, fields: List[str]):
+          super(RedactingFormatter, self).__init__(self.FORMAT)
+          self.fields = fields
+  
+      def format(self, record: logging.LogRecord) -> str:
+          message = super(RedactingFormatter, self).format(record)
+          redacted = obfuscate_message(self.fields, self.REDACTION,
+                                       message, self.SEPARATOR)
+          return redacted
 
 
 def get_logger() -> logging.Logger:
